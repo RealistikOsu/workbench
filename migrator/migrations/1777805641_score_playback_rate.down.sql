f561af7fe54fdb9645be259480eb0fae4d30a155
RealistikDash <realistikdash@gmail.com>
2026-05-03 18:55:07 +0800

    add: playback_Rate field to db

---
+++ migrator/migrations/1777805641_score_playback_rate.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE scores         DROP COLUMN playback_rate;
+ALTER TABLE scores_relax   DROP COLUMN playback_rate;
+ALTER TABLE scores_ap      DROP COLUMN playback_rate;
+ALTER TABLE scores_queued  DROP COLUMN playback_rate;
